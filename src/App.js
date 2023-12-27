92a2f7edc91847813b366851bf75a7772206db5d
Yuto Kobayashi <>
2023-12-27 11:24:58 +0900

    Section 4.46
    Section 4.46

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 function App() {
+    const [count, setCount] = useState(0);
     const handleClick = () => {
-        console.log('Button was clicked!');
+        setCount(count + 1);
     };
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
+            <div>Number of count is: {count}</div>
         </div>
     );
 }

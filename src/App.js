20a94f0e0493587b5b3eccc3def891cc1c583ce2
Yuto Kobayashi <>
2024-01-04 14:23:32 +0900

    Section 4.51
    Section 4.51

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
+
     const handleClick = () => {
-        setCount(count + 1);
+        setAnimals([...animals, getRandomAnimal()]);
     };
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
-            <div>Number of count is: {count}</div>
+            <div>{animals}</div>
         </div>
     );
 }
